66f421db612d8e18c25ee6d6ae03c1ea503dd3cf
Julian Eggers <eggers.julian@gmail.com>
2017-01-07 16:19:08 +0100

    1.0.1-RELEASE

--- src/main/java/com/itelg/devops/jpa/domain/WebHook.java
+++ src/main/java/com/itelg/devops/jpa/domain/WebHook.java
@@ -1,8 +1,12 @@
 package com.itelg.devops.jpa.domain;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
-@Data
+@Getter
+@Setter
+@ToString
 public class WebHook
 {
     private long id;
