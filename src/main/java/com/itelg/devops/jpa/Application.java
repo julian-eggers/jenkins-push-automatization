a452219d50b7d45561b9450f6cc65d94fb5461cc
avidesit <avidesit@avidesit-lp01>
2017-07-21 22:14:01 +0200

    Updates

--- src/main/java/com/itelg/devops/jpa/Application.java
+++ src/main/java/com/itelg/devops/jpa/Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application
 {
-	public static final void main(String[] args) throws Exception
-	{
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Application.class, args);
+    }
 }
